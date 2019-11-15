c18b80da49500ceb72c3b5c9c6f5c907c4d096d6
vivek kumar <vivekkumar27june88@gmail.com>
2019-11-15 17:08:23 +0530

    Creating a test form and using bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSettingsFormsComponent } from './user-settings-forms/user-settings-forms.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserSettingsFormsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-settings-forms/user-settings-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-forms',
+  templateUrl: './user-settings-forms.component.html',
+  styleUrls: ['./user-settings-forms.component.scss']
+})
+export class UserSettingsFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
