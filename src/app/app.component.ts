8f6a5646500d8b34dd89d152f941c1faf8bb8581
vivek kumar <vivekkumar27june88@gmail.com>
2019-11-17 16:11:08 +0530

    Dynamically Adding Form Element using FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'declarative-form';
+  formType = 'showDynamicForm';
   showDeclarativeForm = false;
+  showReactiveForm = false;
+  showDynamicForm = true;
 
-  public checkboxClickHandler(event: Event) {
-    // console.log('TCL: AppComponent -> checkboxClickHandler -> event', event);
-    this.showDeclarativeForm = event.target['checked'];
+  public checkboxClickHandler(event: Event, formType: string) {
+    switch (formType) {
+      case 'showDeclarativeForm':
+        this.showDeclarativeForm = event.target['checked'];
+        this.showReactiveForm = false;
+        this.showDynamicForm = false;
+        break;
+
+      case 'showReactiveForm':
+        this.showReactiveForm = false;
+        this.showReactiveForm = event.target['checked'];
+        this.showDynamicForm = false;
+        break;
+
+      case 'showDynamicForm':
+        this.showDynamicForm = false;
+        this.showReactiveForm = false;
+        this.showDynamicForm = event.target['checked'];
+        break;
+    }
   }
 }
