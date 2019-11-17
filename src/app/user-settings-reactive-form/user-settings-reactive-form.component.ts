64937c153ad5d5f111e3128f2b42cb654dffd5f5
vivek kumar <vivekkumar27june88@gmail.com>
2019-11-17 13:10:33 +0530

    Created reactive form group using FormBuilder

--- src/app/user-settings-reactive-form/user-settings-reactive-form.component.ts
+++ src/app/user-settings-reactive-form/user-settings-reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-reactive-form',
@@ -10,19 +10,19 @@ export class UserSettingsReactiveFormComponent implements OnInit {
   public userSettingForm: FormGroup;
   public formSubmitted = false;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.createForm();
   }
 
   ngOnInit() {}
 
   createForm() {
-    this.userSettingForm = new FormGroup({
-      name: new FormControl('Vivek Kumar', { validators: Validators.required, updateOn: 'change' }),
-      emailOffers: new FormControl(false),
-      interfaceStyle: new FormControl(''),
-      subscriptionType: new FormControl(''),
-      notes: new FormControl('')
+    this.userSettingForm = this.fb.group({
+      name: ['Vivek Kumar', { validators: Validators.required, updateOn: 'change' }],
+      emailOffers: [''],
+      interfaceStyle: [''],
+      subscriptionType: [''],
+      notes: ['']
     });
   }
 
